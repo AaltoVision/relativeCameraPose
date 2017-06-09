61e01c338c9c9deae4c6d12d9807903e59edee06
Melekhov Iaroslav <melekhi1@cs-050.org.aalto.fi>
2017-06-09 11:33:01 +0300

    some modifications

--- pre-trained/download_models.sh
+++ pre-trained/download_models.sh
@@ -1,6 +1,5 @@
+rm hybrid*
 # HybridCNN .prototxt and .caffemodel files
 wget http://places.csail.mit.edu/model/hybridCNN_upgraded.tar.gz
 gunzip < hybridCNN_upgraded.tar.gz | tar xvf -
 rm *.csv *.binaryproto *.tar.gz
-#gunzip < hybridCNN_upgraded.tar.gz | tar xvf - $ gunzip < hybridCNN_upgraded.tgz | tar xvf -
-
